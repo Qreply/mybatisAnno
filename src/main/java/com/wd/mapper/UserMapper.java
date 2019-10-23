55b119133f2b35ddc21595ada65a5315acd9e359
Qreply <1041334031@qq.com>
2019-10-23 17:31:55 +0800

    annotation

--- src/main/java/com/wd/mapper/UserMapper.java
+++ src/main/java/com/wd/mapper/UserMapper.java
@@ -1,13 +1,18 @@
 package com.wd.mapper;
 
 import com.wd.entity.User;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
 
 /**
  * 用户的持久层接口
+ * 在mybatis中针对,CRUD一共有四个注解
+ *  @Select @Insert @Update @Delete
  */
 public interface UserMapper {
 
@@ -17,4 +22,49 @@ public interface UserMapper {
      */
     @Select("select * from user")
     List<User> findAll();
+
+    /**
+     * 保存用户
+     * @param user
+     */
+    @Insert("insert into user (username, address, sex, birthday) values (#{username}, #{address}, #{sex}, #{birthday})")
+    void saveUser(User user);
+
+    /**
+     * 更新用户
+     * @param user
+     */
+    @Update("update user set username = #{username}, address = #{address}, sex = #{sex}, birthday = #{birthday} where id = #{id}")
+    void updateUser(User user);
+
+    /**
+     * 删除用户
+     * @param id
+     */
+    @Delete("delete from user where id = #{id}")
+    void deleteUser(Integer id);
+
+    /**
+     * 根据id查询用户
+     * @param id
+     * @return
+     */
+    @Select("select * from user where id = #{id}")
+    User findById(Integer id);
+
+    /**
+     * 根据用户名称模糊查询
+     * @param name
+     * @return
+     */
+    //@Select("select * from user where username like #{name}")
+    @Select("select * from user where username like '%${value}%' ")
+    List<User> findUserByName(String name);
+
+    /**
+     * 查询总用户数量
+     * @return
+     */
+    @Select("select count(*) from user ")
+    int findTotalUser();
 }
--- src/test/java/com/wd/test/MybatisAnnoTest.java
+++ src/test/java/com/wd/test/MybatisAnnoTest.java
@@ -14,7 +14,7 @@ import java.util.List;
  * mybatis的入门案例
  */
 
-public class MybatisTest {
+public class MybatisAnnoTest {
 
     /**
      * 入门案例
@@ -24,11 +24,10 @@ public class MybatisTest {
         //1.读取配置文件
         InputStream in = Resources.getResourceAsStream("SqlMapConfig.xml");
         //2.创建SqlSessionFactory工厂
-        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
-        SqlSessionFactory factory = builder.build(in);
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
         //3.使用工厂生产SqlSession对象
         SqlSession session = factory.openSession();
-        //4.使用SqlSession创建Dao代理的接口对象
+        //4.使用SqlSession创建Mapper代理的接口对象
         UserMapper userMapper = session.getMapper(UserMapper.class);
         //5.使用代理对象执行方法
         List<User> users = userMapper.findAll();
